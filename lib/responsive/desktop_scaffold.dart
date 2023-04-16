69d4664e69ba72c1776256f9a9d650c3da854c39
mehmetcannn0 <mehmetceraslan@gmail.com>
2023-04-16 23:04:45 +0300

    responsive layout + drawer yapıldı

---
+++ lib/responsive/desktop_scaffold.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard_ui/constants.dart';
+
+class DesktopScaffold extends StatefulWidget {
+  const DesktopScaffold({super.key});
+  @override
+  State<DesktopScaffold> createState() => _DesktopScaffoldState();
+}
+
+class _DesktopScaffoldState extends State<DesktopScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: myAppBar,
+        backgroundColor: myDefaultBackground,
+        body: Row(
+          children: [myDrawer],
+        ));
+  }
+}
---
+++ lib/responsive/tablet_scaffold.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard_ui/constants.dart';
+
+class TabletScaffold extends StatefulWidget {
+  const TabletScaffold({super.key});
+  @override
+  State<TabletScaffold> createState() => _TabletScaffoldState();
+}
+
+class _TabletScaffoldState extends State<TabletScaffold> {
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      appBar: myAppBar,
+      backgroundColor: myDefaultBackground,
+      drawer: myDrawer,
+      body: const Center(
+        child: Text('TabletScaffold'),
+      ),
+    );
+  }
+}
